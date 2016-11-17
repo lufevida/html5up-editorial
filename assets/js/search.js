fc5a827fdecb4cb3d8442b652db21e0de7f323bc
Luis Felipe Vieira Damiani <lufevida@gmail.com>
2016-11-17 17:29:34 -0500

    Commited by RunloopPages on 2016-11-17 22:29:34 +0000

--- assets/js/search.js
+++ assets/js/search.js
@@ -41,7 +41,6 @@
       this.field('id');
       this.field('title', { boost: 10 });
       this.field('author');
-      this.field('category');
       this.field('content');
     });
 
@@ -50,7 +49,6 @@
         'id': key,
         'title': window.store[key].title,
         'author': window.store[key].author,
-        'category': window.store[key].category,
         'content': window.store[key].content
       });
 
